aaca060042054857d56e5cc515142444a6d7de84
kdickson2023 <yeboahkwasidickson@gmail.com>
2023-02-23 04:03:10 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _isupper - Program to check uppercase letters
+* @c: character to check
+*
+* Return: 0 or 1
+*/
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+
+else
+return (0);
+
+}

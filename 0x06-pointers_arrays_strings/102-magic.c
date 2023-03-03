df3489d39b0e3d1c82602b77f7f0a9986b53a89c
kdickson2023 <yeboahkwasidickson@gmail.com>
2023-03-02 16:29:26 -0800

    102-magic.c

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - write your line code
+* Return: 0 (Success)
+*/
+
+int main(void)
+{
+int n;
+int a[5];
+int *p;
+
+a[2] = 1024;
+p = &n;
+
+printf("a[2] = %d\n", a[2]);
+return (0);
+}
+
